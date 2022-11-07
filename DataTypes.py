9e26728e42c297aa4d8d346679ba884987aa34d4
caden <cpete.est2014@gmail.com>
2022-11-07 15:39:56 -0700

    11/7 update

---
+++ DataTypes.py
@@ -0,0 +1,25 @@
+
+#string ("str")  : any character in "" or '' or """word"""
+#example: "they call me"Mr. X"" you can nest quotations in a string
+
+# integer (int)  : 35, -124, 12, 99, -3
+
+# float (float)  : 24, 23.56, 99.999, 0.17, -35.924
+
+# boolean (bool) : 1 or 0, True or False
+
+# collections
+#  -list         : [True, 12, Beth, 94, [-12, tina, False]] an array with any data type in it
+#                  grades = [84, 96, 76.6, 95]
+#                  grades = list((84, 96, 76.6, 95))
+#  -tuple        : ("tim", "Kim", "bob", "Tina") data cannot change after being defined
+#                  grades = (84, 96, 76.6, 95)
+#                  grades = tuple((84, 96, 76.6, 95))
+#  -set          : {84, 96, 76.6, 95} unordered, cannot change unless removing or adding items. No duplicates
+#                  grades = {84, 96, 76.6, 95}
+#                  grades = set((84, 96, 76.6, 95))
+#  -dictionary   : pet = {"type" : "cat", "color" : "orange", "personality" : "sweet"} defines these things for the variable
+#                  (pet["color"])
+#                  (pet["color"]) = "black"
+
+# NoneType (none): None (empty variable)
